ce76c5601238d16f71664755741c3552d2a912be
luciamata5 <mat18013@byui.edu>
2019-11-14 14:20:30 -0700

    update

--- index.js
+++ index.js
@@ -8,3 +8,42 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  app.get('/calculate', handleCalculation);
+
+  // MODEL
+  function handleCalculation(req, res) {
+    const weight = Number(request.query.weight);
+    const packageType = request.query.packageType;
+
+    calculateRate(weight, packageType);
+  }
+
+  function calculateRate (weight, packageType) {
+    
+    let result = 0;
+
+    if (packageType = "Letters (Stamped)") {
+      result = .40 + (weight * .15);
+    }
+    else if (packageType = "Letters (Metered)") {
+      result = .35 + (weight * .15);
+    }
+    else if (packageType = "Large Envelopes (Flats)") {
+      result = .85 + (weight * .15);
+    }
+    else if (packageType = "First-Class Package Service—Retai") {
+      if (weight <= 4) { result = 3.66; }
+      else if (weight <= 8) { result = 4.39; }
+      else if (weigth <= 12) { result = 5.19; }
+      else {result = 5.71}
+    }
+    else {
+      result = 'Error';
+    }
+
+    const params = {result : result};
+
+    res.render('result', params);
+
+  }
